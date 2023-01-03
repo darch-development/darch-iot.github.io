8f916a10a671e559431eb879502f9d6102a8dc69
satilpereira <satil.prog@gmail.com>
2023-01-03 12:33:32 -0300

    fixing, adding src/scripts

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+burguer_menu = () => {
+    const burguer = document.getElementsByClassName('navbar__container__menu');
+    burguer[0].classList.toggle("active");
+    // burguer[1].classList.toggle("active");
+    // burguer[2].classList.toggle("active");
+}
